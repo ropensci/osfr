7ab8786772eb2ee93715a0074a0e2e7b001ad357
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-09-11 23:08:25 +0200

    Add standard link construction for all types.

--- R/construct_link.R
+++ R/construct_link.R
@@ -1,8 +1,17 @@
-construct.link <- function(request, type){
+construct.link <- function(id, type){
   base <- "https://staging2-api.osf.io/v2/"
 
   if (type == "applications")
-    result <- sprintf("%s%s", base, link.applications(request))
+    result <- sprintf("%s%s", base, link.applications(id))
+
+  if (type == "nodes")
+    result <- sprintf("%s%s", base, link.nodes(id))
+
+  if (type == "users")
+    result <- sprintf("%s%s", base, link.users(id))
+
+  if (type == "files")
+    result <- sprintf("%s%s", base, link.files(id))
 
   return(result)
 }
@@ -14,13 +23,19 @@ link.applications <- function(client_id){
 }
 
 link.nodes <- function(node_id){
+  result <- sprintf("nodes/%s", node_id)
 
+  return(result)
 }
 
 link.users <- function(user_id){
+  result <- sprintf("users/%s", user_id)
 
+  return(result)
 }
 
 link.files <- function(file_id){
+  result <- sprintf("files/%s", file_id)
 
+  return(result)
 }
