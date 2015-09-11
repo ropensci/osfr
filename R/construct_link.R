91dce1d6b524ee8ebc442049ad6c103aee910147
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-09-11 22:56:46 +0200

    Add arguments to link.applications();link.files() to correspond to OSF API

--- R/construct_link.R
+++ R/construct_link.R
@@ -2,7 +2,7 @@ construct.link <- function(request, type){
   base <- "https://staging2-api.osf.io/v2/"
 }
 
-link.applications <- function(){}
-link.nodes <- function(){}
+link.applications <- function(client_id){}
+link.nodes <- function(node_id){}
 link.users <- function(user_id){}
-link.files <- function(){}
+link.files <- function(file_id){}
