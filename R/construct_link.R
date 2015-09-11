e0fd76a6e79deb3175e555850e0af2735ecbd82d
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-09-11 22:40:50 +0200

    Add skeleton functions for link construction types.

--- R/construct_link.R
+++ R/construct_link.R
@@ -1,3 +1,8 @@
 construct.link <- function(request, type){
   base <- "https://staging2-api.osf.io/v2/"
 }
+
+link.applications <- function(){}
+link.nodes <- function(){}
+link.users <- function(){}
+link.files <- function(){}
