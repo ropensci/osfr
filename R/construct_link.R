0046cdeababc47074a9390e6d31feb362afcd093
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-09-11 23:02:46 +0200

    Add link building for applications.

--- R/construct_link.R
+++ R/construct_link.R
@@ -1,8 +1,26 @@
 construct.link <- function(request, type){
   base <- "https://staging2-api.osf.io/v2/"
+
+  if (type == "applications")
+    result <- sprintf("%s%s", base, link.applications(request))
+
+  return(result)
 }
 
-link.applications <- function(client_id){}
-link.nodes <- function(node_id){}
-link.users <- function(user_id){}
-link.files <- function(file_id){}
+link.applications <- function(client_id){
+  result <- sprintf("applications/%s", client_id)
+
+  return(result)
+}
+
+link.nodes <- function(node_id){
+
+}
+
+link.users <- function(user_id){
+
+}
+
+link.files <- function(file_id){
+
+}
