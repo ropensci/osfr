95655778a1f3c2956ae67c074a31e676019a0cbe
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-09-12 17:17:11 +0200

    Add construct.link() documentation.

--- R/construct_link.R
+++ R/construct_link.R
@@ -1,3 +1,10 @@
+#' Construct an API link with proper base
+
+#' @param request The request link to be combined with the base API link.
+#' @return The full request link with proper base
+#' @examples
+#' construct.link("nodes/NODE_ID/files/")
+
 construct.link <- function(request = NULL){
   base <- "https://staging2-api.osf.io/v2/"
 
