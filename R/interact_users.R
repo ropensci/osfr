2f4b35d4035256fc29ecfd77c01056c908a53ef0
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-09-12 17:22:26 +0200

    Add documentation users.all()

--- R/interact_users.R
+++ R/interact_users.R
@@ -1,3 +1,27 @@
+#' Retrieve all users and their information from the OSF API
+#'
+#' @return Object dataframe including, for each user:
+#' \enumerate{
+#' \item id
+#' \item fullname
+#' \item given_name
+#' \item middle_names
+#' \item family_name
+#' \item suffix
+#' \item date_registered
+#' \item profile_image_url
+#' \item gitHub
+#' \item scholar
+#' \item personal_website
+#' \item twitter
+#' \item linkedIn
+#' \item impactStory
+#' \item orcid
+#' \item researcherId
+#' \item links
+#' \item nodes
+#' }
+
 users.all <- function(){
   raw <- GET(construct.link("users"))
 
