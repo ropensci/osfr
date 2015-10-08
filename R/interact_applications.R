c501c5bf75adc1016b2cdcf820541ddd54f52628
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-10-08 18:58:35 +0200

    Update pckg calls

--- R/interact_applications.R
+++ R/interact_applications.R
@@ -5,13 +5,13 @@ get.applications <- function(id = NULL, user = NULL, password = NULL){
     warning("Please input password")}
 
   if (is.null(id)){
-    raw <- GET(construct.link("applications"), authenticate(user, password))
+    raw <- httr::GET(construct.link("applications"), authenticate(user, password))
 
-    result <- fromJSON(content(raw, 'text'))
+    result <- rjson::fromJSON(content(raw, 'text'))
   } else {
-    raw <- GET(construct.link(paste0("applications/?filter[id]=", id)), authenticate(user, password))
+    raw <- httr::GET(construct.link(paste0("applications/?filter[id]=", id)), authenticate(user, password))
 
-    result <- fromJSON(content(raw, 'text'))
+    result <- rjson::fromJSON(content(raw, 'text'))
   }
 
   return(result)
