1b5a0e8ebb1db9986ada3da200c8dbf2fff8c722
CHJ Hartgerink <c.hartgerink@gmail.com>
2015-10-16 20:11:56 +0200

    add put.nodes() unit tests

--- tests/testthat/test_nodes.R
+++ tests/testthat/test_nodes.R
@@ -88,6 +88,22 @@ test_that("post.nodes operates properly", {
 		    description = "test", category = "hypofdthesis"), throws_error("Please input proper category, see documentation"))
 	})
 
+test_that("put.nodes operates properly", {
+	expect_that(class(put.nodes(id = "jqdn2", user = 'h.schwarzenegger@gmail.com',
+		 password = 'testingtesting',
+		  type = 'nodes',
+		   title = 'From tests',
+		    description = NULL,
+		     category = 'project',
+		      public = 'true',
+		       tags = c("tfadgfdagdf", "tgfhdjkga"))), matches('list'))
+	expect_that(put.nodes(id = "jqdn2", user = 'h.schwarzenegger@gmail.com',
+		 password = 'testingtesting',
+		  type = 'nodes',
+		   title = 'From tests',
+		    description = "test", category = "hypofdthesis"), throws_error("Please input proper category, see documentation"))
+	})
+
 test_that("nodes are deleted properly", {
 	x = get.users(id = 'me', user = 'h.schwarzenegger@gmail.com', password = 'testingtesting')
 	y = rjson::fromJSON(httr::content(httr::GET(x$data$relationships$nodes$links$related$href), 'text'))
