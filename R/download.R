64c75ed3ec99bf5c66d5990f5683c6d8e069760c
CHJ Hartgerink <chjh@protonmail.com>
2016-04-29 21:31:24 +0200

    Add file name extraction to download.osf

--- R/download.R
+++ R/download.R
@@ -1,8 +1,8 @@
 #' Download files from the OSF
 #'
 #' @param id Specify the node id (osf.io/XXXX)
+#' @param file Specify path to save file. If NULL, defaults to OSF
 #' @param private Boolean to specify whether file is private
-#' @param file Specify path to save file
 #'
 #' @return
 #' @export
@@ -37,6 +37,15 @@ download.osf <- function(id = NULL,
   }
 
   res <- rjson::fromJSON(httr::content(call, 'text'))
+
+  # Find the filename as on the OSF
+  if (is.null(file))
+  {
+    txt <- res$data$links$download
+    start <- tail(gregexpr('/', txt)[[1]], 1)
+    end <-  nchar(txt)
+    file <- substr(txt, start + 1, end)
+  }
 
   httr::GET(res$data$links$download,
             httr::write_disk(file, overwrite = TRUE))
