8ab66acfb43442b6367b6754b3d406c57bab0884
CHJH <chjh@protonmail.com>
2016-09-21 18:03:26 +0200

    Add test for equivalence in separate search functions and meta search function

--- tests/testthat/test_search.R
+++ tests/testthat/test_search.R
@@ -2,12 +2,19 @@ context("Test the search functions")
 
 test_that("searching osf", {
   # Make sure that multiword searches operate
-  expect_that(class(search.osf(title = 'ecological validity')), matches("data.frame"))
-  expect_error(search.osf(title = '*@#&#ASD'))
+  expect_that(class(search.nodes(title = 'ecological validity')), matches("data.frame"))
+  expect_error(search.nodes(title = '*@#&#ASD'))
 })
 
 test_that("searching osf users", {
   # Make sure that multiword searches operate
   expect_that(class(search.users(family_name = 'nosek')), matches("data.frame"))
   expect_that(class(search.users(family_name = 'spies')), matches("data.frame"))
+})
+
+test_that("meta search = separate search", {
+  expect_that(search.osf(type = 'nodes', title = 'ecological validity'),
+              equals(search.nodes(title = 'ecological validity')))
+  expect_that(search.osf(type = 'users', family_name = 'nosek'),
+              equals(search.users(family_name = 'nosek')))
 })
