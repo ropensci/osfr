dec189cc8cb942ee7e3d8cd33998b152f86c0a0a
CHJ Hartgerink <chjh@protonmail.com>
2017-09-22 15:52:14 +0200

    Update comments.R for consistency

--- R/comments.R
+++ R/comments.R
@@ -1,6 +1,10 @@
 #' Comment on a project on the OSF
 #'
-#' @param id OSF id (osf.io/XXXX)
+#' Provide comments on an OSF project. Please be respectful and inclusive in doing
+#' so and see the Terms of Use of the OSF. 
+#' \link{https://github.com/CenterForOpenScience/cos.io/blob/master/TERMS_OF_USE.md}
+#' 
+#' @param id OSF id (osf.io/XXXX; just XXXX)
 #' @param txt Contents of the comment
 #'
 #' @return Boolean, posting succeeded?
@@ -8,16 +12,22 @@
 #'
 #' @examples
 #' \dontrun{comment_osf(id = '12345', txt = 'This is an example')}
+
 comment_osf <- function(id = NULL, txt) {
 
   config <- get_config(TRUE)
 
-  if (is.null(txt))
+  if (is.null(txt)) {
     stop("Empty comment? Seems redundant. Use the txt argument")
-  if (is.null(id))
-    stop("Enter id to post comment to (osf.io/XXXX)")
-  if (process_type(id) != "nodes")
-    stop("Currently unable to post comments to files.")
+  }
+  
+  if (is.null(id)) {
+      stop("Enter id to post comment to (osf.io/XXXX)")
+  }
+
+  if (process_type(id) != "nodes") {
+      stop("Currently unable to post comments to files.")
+  }
 
   url_osf <- construct_link(sprintf("nodes/%s/comments/", id))
 
@@ -26,14 +36,10 @@ comment_osf <- function(id = NULL, txt) {
     data = list(
       type = "comments",
       attributes = list(
-        content = txt
-      ),
+        content = txt),
       relationships = list(
         target = list(
-          data = list(
-            type = "nodes",
-            id = id
-          )
+          data = list(type = "nodes", id = id)
         )
       )
     )
@@ -41,8 +47,9 @@ comment_osf <- function(id = NULL, txt) {
 
   call <- httr::POST(url = url_osf, body = comment, encode = "json", config)
 
-  if (!call$status_code == 201)
-    stop(sprintf("Posting comment to node %s failed.", id))
+  if (!call$status_code == 201) {
+      stop(sprintf("Posting comment to node %s failed.", id))
+    }
 
   return(TRUE)
 }
