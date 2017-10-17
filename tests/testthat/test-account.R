5b95e78daab0a4e7b4b8f802d76cc3f64aff8720
CHJ Hartgerink <chjh@protonmail.com>
2017-10-17 15:29:45 +0200

    Residual commit

--- tests/testthat/test-account.R
+++ tests/testthat/test-account.R
@@ -4,11 +4,11 @@ test_that('login works', {
   expect_is(login(pat = Sys.getenv('OSF_PAT')), 'character')
   expect_is(login(), 'character')
 })
-#
+
 # test_that('welcome works', {
 #   expect_null(welcome())
 # })
-#
+
 # test_that('logout works', {
 #   expect_true(logout())
 # })
