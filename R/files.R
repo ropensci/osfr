080b995fa4d292ea6e7524494eed455428614470
CHJ Hartgerink <chjh@protonmail.com>
2017-10-18 18:40:48 +0200

    Intermittent overhaul of files
    Replacing all double-quotes w single quotes, rechecked upload_new, upload_revision, delete_files, delete process_file_id (moved to utils in previous commit)

--- R/files.R
+++ R/files.R
@@ -1,11 +1,14 @@
 #' Upload a file to the OSF (both new and revised)
 #'
-#' @param id OSF id (osf.io/XXXX; just XXXX) to upload to. Specify project to upload new file,
-#'  specify a file to upload a revision
+#' @param id OSF id (osf.io/XXXX; just XXXX) to upload to. Specify project to
+#' upload a new file, specify a file to upload a revision.
 #' @param path Path to file on local machine to upload.
-#' @param dest Name of the destination file on OSF (if \code{NULL}, \code{basename(path)} will be used). Note that this can be used to specify what folder to place files in, e.g. "my_folder/file.png". Also note that if \code{id} is a file ID, this is not necessary.
+#' @param dest Name of the destination file on OSF (if \code{NULL},
+#' \code{basename(path)} will be used). Note that this can be
+#'  used to specify what folder to place files in, e.g. 'my_folder/file.png'.
+#'  Also note that if \code{id} is a file ID, this is not necessary.
 #'
-#' @return Boolean of upload success
+#' @return Link if new file created
 #' @export
 #'
 #' @examples
@@ -13,29 +16,30 @@
 #' upload_file(id = '12345', path = 'test.pdf')
 #' }
 
-upload_file <- function(id, path, dest = NULL) {
+upload_files <- function(id, path, dest = NULL) {
   type <- process_type(id, private = TRUE)
 
-  if (is.null(dest))
+  if (is.null(dest)) {
     dest <- basename(path)
+  }
 
-  if (type == "nodes") {
-    fi <- get_file_info(id, private = TRUE)
+  if (type == 'nodes') {
+    fi <- get_files_info(id, private = TRUE) # why is this necessary? too many calls it seems
     idx <- which(fi$materialized == pre_slash(dest))
     if (length(idx) != 1) {
-      message("Creating new file on OSF...")
+      message('Creating new file on OSF...')
       upload_new(id, path, dest)
     } else {
-      message("Revising file on OSF...")
+      message('Revising file on OSF...')
       upload_revision(id, path, dest, fi)
     }
-  } else if (type == "files") {
-    message("Revising file...")
+  } else if (type == 'files') {
+    message('Revising file...')
     upload_revision(id, path, dest)
   } else {
-    stop("Something odd happened.\n
-  If the problem persists, consider issuing a bug report on
-  github.com/chartgerink/osfr")
+    stop('Something odd happened.\n
+          If the problem persists, consider issuing a bug report on
+          github.com/chartgerink/osfr')
   }
 }
 
@@ -44,7 +48,7 @@ upload_file <- function(id, path, dest = NULL) {
 #' @param id OSF id (osf.io/XXXX) to upload to. Specify project to upload new file,
 #'  specify a file to upload a revision
 #' @param path Path to directory on local machine to zip up and upload.
-#' @param dest Name of the destination file on OSF (if \code{NULL}, \code{basename(path)} with a ".zip" suffix will be used). Note that this can be used to specify what folder to place files in, e.g. "my_folder/my_directory.zip". Also note that if \code{id} is a file ID, this is not necessary.
+#' @param dest Name of the destination file on OSF (if \code{NULL}, \code{basename(path)} with a '.zip' suffix will be used). Note that this can be used to specify what folder to place files in, e.g. 'my_folder/my_directory.zip'. Also note that if \code{id} is a file ID, this is not necessary.
 #'
 #' @return Boolean of upload success
 #' @export
@@ -58,173 +62,157 @@ upload_file <- function(id, path, dest = NULL) {
 upload_zip <- function(id, path, dest = NULL) {
 
   if (!dir.exists(path))
-    stop("Please specify a valid directory to zip.")
+    stop('Please specify a valid directory to zip.')
 
-  zp <- tempfile(fileext = ".zip")
+  zp <- tempfile(fileext = '.zip')
 
-  message("Zipping to ", zp, "...")
+  message('Zipping to ', zp, '...')
   zip(zp, path)
 
   if (is.null(dest))
-    dest <- paste0(basename(normalizePath(path)), ".zip")
+    dest <- paste0(basename(normalizePath(path)), '.zip')
 
-  if (!grepl("\\.zip$", dest))
-    dest <- paste0(dest, ".zip")
+  if (!grepl('\\.zip$', dest))
+    dest <- paste0(dest, '.zip')
 
   upload_file(id, zp, dest)
 }
 
-# Upload a new file to the OSF.
-#
-# @param id Parent OSF project id (osf.io/XXXX) to upload to.
-# @param path Path to file on local machine to upload.
-# @param dest Name of the destination file on OSF (if \code{NULL}, \code{basename(path)} will be used). Note that this can be used to specify what folder to place files in, e.g. "my_folder/file.png".
-#
-# @return Waterbutler URL
-# @seealso \code{\link{upload_file}}, \code{\link{upload_revision}}
-upload_new <- function(id, path, dest = NULL) {
+#' Upload a new file to the OSF.
+#'
+#' @param id Parent OSF project id (osf.io/XXXX) to upload to.
+#' @param path Path to file on local machine to upload. Ensure file has
+#' proper extension named (i.e., extension sensitive, not like on Linux)
+#' @param name Name of the destination file on OSF (if \code{NULL},
+#' \code{basename(path)} will be used).
+#'
+#' @return Waterbutler URL
+#' @seealso \code{\link{upload_file}}, \code{\link{upload_revision}}
 
-  if (!file.exists(path))
-    stop("File ", path, " does not exist on local machine.")
+upload_new <- function(id, path, name = NULL) {
 
-  if (is.null(dest))
-    dest <- basename(path)
+  if (!file.exists(path)) {
+    stop(sprintf('File %s does not exist on local machine.', path))
+  }
+
+  if (is.null(name)) {
+    name <- basename(path)
+  }
 
   config <- get_config(TRUE)
 
-  # Assume it is private just in case
-  # Incorporates login check needed anyway
   typ <- process_type(id, private = TRUE)
-  if (typ != "nodes")
-    stop("Cannot upload new file if no node ID is specified.")
-
-  # Handle the case of file nested in folder(s)
-  if (length(strsplit(dest, "\\/")[[1]]) > 1) {
-    fi <- get_file_info(id, private = TRUE)
-    dnm <- paste0(dirname(dest), "/")
-    dnm <- pre_slash(dnm)
-    idx <- which(fi$materialized == dnm)
-    if (length(idx) != 1 && dirname(dnm) != "/")
-      stop("Cannot create file ", dest, " because parent directory ",
-        dirname(dest), " doesn't exist on server.")
-
-    url_osf <- paste0(fi$href[idx], "?kind=file&name=", basename(dest))
-  } else {
-    url_osf <- construct_link_files(id,
-      request = paste0("?kind=file&name=", dest))
+  if (typ != 'nodes') {
+    stop('Cannot upload new file if no node ID is specified.')
   }
 
-  url_osf <- gsub(url_osf, pattern = "\\s", replacement = "%20", perl = TRUE)
+  url_osf <- construct_link_files(id, request = paste0('?kind=file&name=',
+                                                       name))
+  # Ensure proper spaces in URL
+  url_osf <- gsub(url_osf, pattern = '\\s', replacement = '%20', perl = TRUE)
 
   call <- httr::PUT(url_osf, body = httr::upload_file(path),
-    encode = "raw", config = config)
+                    encode = 'raw', config = config)
 
-  if (call$status_code == 409)
-    stop("Conflict in path naming. Please use upload_revision or change path")
-  if (call$status_code != 201)
-    stop("Unsuccessful upload.")
+  if (call$status_code == 409) {
+    stop('Conflict in path naming. Please use upload_revision or change path')
+  } else if (call$status_code != 201) {
+    stop('Unsuccessful upload.')
+  }
 
   res <- process_json(call)
 
   return(res$data$links$download)
 }
 
-# Upload a revised file to the OSF
-#
-# @param id OSF id (osf.io/XXXX) of file to revise
-# @param path Path to file on local machine to upload.
-# @param dest Name of the destination file on OSF to be revised (if \code{NULL}, \code{basename(path)} will be used). Note that this can be used to specify what folder to place files in, e.g. "my_folder/file.png". Also note that if \code{id} is a file ID, this is not necessary
-#
-# @return Boolean of revision success
-# @seealso \code{\link{upload_file}}, \code{\link{upload_new}}
-upload_revision <- function(id, path, dest = NULL, fi = NULL) {
+#' Upload a revised file to the OSF
+#'
+#' @param id OSF id (osf.io/XXXX; just XXXX) of file to revise
+#' @param path Path to file on local machine to upload.
+#'
+#' @return Boolean, revision success? (invisible)
+#' @seealso \code{\link{upload_file}}, \code{\link{upload_new}}
+
+upload_revision <- function(id, path) {
+
+  if (!file.exists(path)) {
+    stop(sprintf('File %s does not exist on local machine.', path))
+  }
+
   config <- get_config(TRUE)
   typ <- process_type(id, private = TRUE)
 
-  upload_osf <- ""
-  if (typ == "nodes") {
-    if (is.null(dest))
-      stop("Must specify a path when revising a file using a project ID.")
-    if (is.null(fi))
-      fi <- get_file_info(id, private = TRUE)
-    dest <- pre_slash(dest)
-    idx <- which(fi$materialized == dest)
-    if (length(idx) != 1)
-      stop("Could not find file '", dest,
-        "'on OSF. Please use get_file_info(\"", id,
-        "\") to see what files are available to delete")
-    upload_osf <- paste0(fi$href[idx], "?kind=file")
-  } else if (typ == "files") {
-    url_osf <- construct_link(paste(typ, id, sep = "/"))
+  if (typ == 'nodes') {
+    stop('Specify an OSF id referring to a file.')
+  } else if (typ == 'files') {
+    url_osf <- construct_link(paste(typ, id, sep = '/'))
     call <- httr::GET(url_osf, config)
     res <- process_json(call)
     upload_osf <- res$data$links$upload
+  } else {
+    stop('Unknown error occurred. Please file issue on GitHub.')
   }
 
-  upload <- httr::PUT(
+  call <- httr::PUT(
     upload_osf,
     body = httr::upload_file(path),
-    encode = "raw",
+    encode = 'raw',
     config = config)
 
-  if (upload$status_code != 200)
-    stop("Failed to upload revision")
+  if (call$status_code != 200) {
+    stop('Failed to upload revision')
+  }
 
-  res <- process_json(upload)
-
-  return(res$data$links$download)
+  invisible(TRUE)
 }
 
-#' Delete a file based on OSF file id or a project id + path
+#' Delete a file based on OSF id
 #'
-#' @param id OSF id (osf.io/XXXX) for either a file or a project
-#' @param path if \code{id} is a project ID, the path to the file must be specified as well
+#' @param id OSF id (osf.io/XXXX; just XXXX)
 #'
 #' @return Boolean, delete succeeded?
 #' @export
-delete_file <- function(id, path = NULL) {
+
+delete_files <- function(id) {
+
   config <- get_config(TRUE)
 
-  if (is_valid_osf_id(id) && is.null(path)) {
-    url_osf <- process_file_id(id)
-    if (is.null(url_osf))
-      stop("Could not find a file with associated id '", id, "'.")
-    call <- httr::DELETE(url = url_osf, config)
-  } else {
-    if (is.null(path))
-      stop("Must specify a path when deleting a file using a project ID.")
-    fi <- get_file_info(id, private = TRUE)
-    path <- pre_slash(path)
-    idx <- which(fi$materialized == path)
-    if (length(idx) != 1)
-      stop("Could not find file '", path,
-        "' on OSF. Please use get_file_info(\"", id,
-        "\") to see what files are available to delete")
-    call <- httr::DELETE(url = fi$href[idx], config)
+  if (!is_valid_osf_id(id)) {
+    stop('Please insert valid OSF id.')
   }
 
-  if (call$status_code != 204)
-    stop("Failed to delete file ", id, ". Is it surely a file?", call. = FALSE)
+  url_osf <- process_file_id(id)
+
+  if (is.null(url_osf)) {
+      stop(sprintf('Could not find a file with associated id %s.', id))
+  }
+
+  call <- httr::DELETE(url = url_osf, config)
+
+  if (call$status_code != 204) {
+    stop(sprintf('Failed to delete file %s.', id), call. = FALSE)
+  }
 
   invisible(TRUE)
 }
 
 #' Move (and copy) files on the OSF
 #'
-#' @param from OSF file id to move (osf.io/xxxx)
+#' @param from OSF file id to move (osf.io/xxxx; just xxxx)
 #' @param to OSF id to move to (osf.io/xxxx; needs to be component)
 #' @param filename Optional, rename the file
-#' @param action Move or copy
+#' @param action 'move' or 'copy'
 #' @param conflict Keep old file or replace in case of conflict
 #'
-#' @return Boolean, moving succeeded?
+#' @return Boolean, moving succeeded? (invisible)
 #' @export
-move_file <- function(
+
+move_files <- function(
   from = NULL,
   to = NULL,
   filename = NULL,
-  action = "move",
-  conflict = "replace") {
+  action = 'move',
+  conflict = 'replace') {
 
   config <- get_config(TRUE)
 
@@ -232,36 +220,36 @@ move_file <- function(
     typfrom <- process_type(id = from)
     typto <- process_type(id = to)
 
-    if (typfrom != "nodes" && typto != "nodes")
-      stop("File needs to move from node to node")
+    if (typfrom != 'nodes' && typto != 'nodes')
+      stop('File needs to move from node to node')
 
     url_osf <- process_file_id(from)
   } else {
     typto <- process_type(id = to)
 
-    if (typto != "nodes")
-      stop("File needs to move from node to node")
+    if (typto != 'nodes')
+      stop('File needs to move from node to node')
 
     url_osf <- from
   }
 
   body <- list(
     action = action,
-    path = "/",
+    path = '/',
     rename = filename,
     conflict = conflict,
-    provider = "osfstorage",
+    provider = 'osfstorage',
     resource = to)
 
   call <- httr::POST(
     url_osf,
-    body = body, encode = "json",
+    body = body, encode = 'json',
     config)
 
   if (call$status_code != 201 && call$status_code != 200)
-    stop("Error in moving/copying file '", from, "' to component '", to, "'.")
+    stop('Error in moving/copying file, from to component to')
 
-  return(TRUE)
+  invisible(TRUE)
 }
 
 #' Download files from the OSF
@@ -279,65 +267,48 @@ move_file <- function(
 #' @importFrom utils tail
 #' @export
 
-download_file <- function(id, path = NULL, private = FALSE, version = NULL) {
+download_files <- function(id, path = NULL, private = FALSE, version = NULL) {
   config <- get_config(private)
 
-  url_osf <- construct_link(paste0("guids/", id))
+  url_osf <- construct_link(paste0('guids/', id))
 
   call <- httr::GET(url_osf, config)
 
   if (!call$status_code == 200)
-    stop("Failed. Are you sure you have access to the file?")
+    stop('Failed. Are you sure you have access to the file?')
 
   res <- process_json(call)
 
   # Find the filename as on the OSF
   if (is.null(path)) {
     txt <- res$data$attributes$name
-    start <- utils::tail(gregexpr("/", txt)[[1]], 1)
+    start <- utils::tail(gregexpr('/', txt)[[1]], 1)
     end <-  nchar(txt)
     file <- substr(txt, start + 1, end)
   } else {
-  file <- paste0(path, res$data$attributes$name)
+    file <- paste0(path, res$data$attributes$name)
   }
 
-  message(paste0("Saving to filename: ", file))
+  message(paste0('Saving to filename: ', file))
 
   if (is.null(version)) {
     call <- httr::GET(res$data$links$download, config,
-            httr::write_disk(file, overwrite = TRUE))
+                      httr::write_disk(file, overwrite = TRUE))
   } else {
-    call <- httr::GET(paste0(res$data$links$download, "?revision=", version), config,
+    call <- httr::GET(paste0(res$data$links$download, '?revision=', version), config,
                       httr::write_disk(file, overwrite = TRUE))
   }
   if (call$status_code == 404) {
-    stop("Version of file does not exist.")
-  }
-  
-  if (call$status_code != 200) {
-    stop("Failed to download file.")
+    stop('Version of file does not exist.')
   }
 
-  message("Successfully downloaded file.")
+  if (call$status_code != 200) {
+    stop('Failed to download file.')
+  }
+
+  message('Successfully downloaded file.')
 
   return(file)
-}
-
-#' Processing a file id to waterbutler
-#'
-#' @param id OSF id (osf.io/xxxx)
-#' @param private Boolean, if file is private
-#'
-#' @return Waterbutler URL
-process_file_id <- function(id = NULL, private = FALSE) {
-
-  config <- get_config(private)
-
-  url_osf <- construct_link(paste0("files/", id))
-  call <- httr::GET(url = url_osf, config)
-  res <- process_json(call)
-
-  return(res$data$links$download)
 }
 
 #' Get data frame of information about all files in an OSF node
@@ -347,7 +318,7 @@ process_file_id <- function(id = NULL, private = FALSE) {
 #'
 #' @export
 
-get_file_info <- function(id, private = FALSE) {
+get_files_info <- function(id, private = FALSE) {
 
   config <- get_config(private)
 
@@ -374,20 +345,20 @@ get_file_info <- function(id, private = FALSE) {
     }))
   }
 
-  url_osf <- construct_link_files(id = id, request = "?meta=")
+  url_osf <- construct_link_files(id = id, request = '?meta=')
   call <- httr::GET(url_osf, config)
   files <- process_json(call)$data
   files <- process_files(files)
 
   if (is.null(files)) {
-      return(NULL)
+    return(NULL)
   }
   while (TRUE) {
-    idx <- which(files$kind == "folder" & !files$processed)
+    idx <- which(files$kind == 'folder' & !files$processed)
     if (length(idx) == 0)
       break;
     res <- lapply(files$href[idx], function(href) {
-      href <- paste0(href, "?kind=file&meta=")
+      href <- paste0(href, '?kind=file&meta=')
       call <- httr::GET(href, config)
       tmp <- process_json(call)$data
       process_files(tmp)
