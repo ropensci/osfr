c17dba7f23cc399e66fbb5002b7e1d948ca95cc4
CHJ Hartgerink <chjh@protonmail.com>
2017-11-07 10:32:25 +0100

    Add package doc (#28) and regenerate

---
+++ R/osfr.R
@@ -0,0 +1,12 @@
+#' osfr: A package for interacting with the Open Science Framework.
+#'
+#' The osfr package helps you document your research as you go, which
+#' includes creating projects, uploading files, cloning projects to your
+#' desktop in one fell swoop, and much more.
+#'
+#' Suggestions for improvements or bug reports always welcome at
+#' https://github.com/chartgerink/osfr
+#'
+#' @docType package
+#' @name osfr
+NULL
