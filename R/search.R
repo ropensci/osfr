84ba6e2143d810d0f760731f308e2a82beee9266
Brian Richards <brian.g.richards@gmail.com>
2018-02-22 10:22:40 -0500

    Add explicit encoding for httr::content() call in search_nodes().

--- R/search.R
+++ R/search.R
@@ -53,7 +53,7 @@ search_nodes <- function(
   while (!is.null(res$links$`next`)) {
     whilst <- rjson::fromJSON(
       httr::content(
-        httr::GET(res$links$`next`), "text"))
+        httr::GET(res$links$`next`), "text", encoding = "UTF-8"))
     res$data <- c(res$data, whilst$data)
     res$links$`next` <- whilst$links$`next`
     message(paste0(res$links$`next`))
