8ace9ca34f26eee28e14148a0a5aa856c73fbddb
Brian Richards <brian.g.richards@gmail.com>
2018-03-22 10:51:46 -0400

    Fix typo.

--- R/files.R
+++ R/files.R
@@ -268,7 +268,7 @@ move_files <- function(
 #' @importFrom utils tail
 #' @export
 
-download_files <- function(id, path = NULL, view-only = NULL, version = NULL) {
+download_files <- function(id, path = NULL, view_only = NULL, version = NULL) {
   config <- get_config(private)
 
   url_osf <- construct_link(paste0('guids/', id))
