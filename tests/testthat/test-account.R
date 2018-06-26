abeabde8e9734ccfce0bcea7a717aca315f9a389
Brian Richards <brian.g.richards@gmail.com>
2018-06-26 10:09:54 -0400

    Add testing for login()
    These tests check that login returns an error if it does not have the pat argument. It also tests that login changes the system environment OSF_PAT variable to the provided PAT argument.

--- tests/testthat/test-account.R
+++ tests/testthat/test-account.R
@@ -1,5 +1,15 @@
 context('account operations')
 
+test_that("login works", {
+  # Get an error if login does not have a PAT
+  expect_error(login())
+
+  # Login changes the environmental variable to the PAT
+  Sys.setenv(OSF_PAT = "")
+  login(pat = osf_pat)
+  expect_equal(osf_pat, Sys.getenv("OSF_PAT"))
+})
+
 # test_that('login works', {
 #   expect_is(login(), 'character') # make sure login works when PAT is in env
 #   expect_is(login(pat = Sys.getenv('OSF_PAT')), 'character')
