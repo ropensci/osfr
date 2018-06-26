c2c115e32c207f6a074ca952e24ae6b5e6dc5b10
Brian Richards <brian.g.richards@gmail.com>
2018-06-26 10:16:58 -0400

    Remove old and unused tests

--- tests/testthat/test-account.R
+++ tests/testthat/test-account.R
@@ -22,17 +22,3 @@ test_that("logout works", {
   logout()
   expect_equal(Sys.getenv("OSF_PAT"), "")
 })
-
-# test_that('login works', {
-#   expect_is(login(), 'character') # make sure login works when PAT is in env
-#   expect_is(login(pat = Sys.getenv('OSF_PAT')), 'character')
-#   expect_is(login(), 'character')
-# })
-
-# test_that('welcome works', {
-#   expect_null(welcome())
-# })
-
-# test_that('logout works', {
-#   expect_true(logout())
-# })
