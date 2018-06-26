934fa2d67429b34d20a2ccef6dd238a58ee9de71
Brian Richards <brian.g.richards@gmail.com>
2018-06-26 10:02:40 -0400

    Comment out old tests
    These tests will need to be rewritten to incorporate changes to the package. Tests that make calls to the OSF API will need to implement mocking so that the tests will remain stable over time.

--- tests/testthat/test-account.R
+++ tests/testthat/test-account.R
@@ -1,10 +1,10 @@
 context('account operations')
 
-test_that('login works', {
-  expect_is(login(), 'character') # make sure login works when PAT is in env
-  expect_is(login(pat = Sys.getenv('OSF_PAT')), 'character')
-  expect_is(login(), 'character')
-})
+# test_that('login works', {
+#   expect_is(login(), 'character') # make sure login works when PAT is in env
+#   expect_is(login(pat = Sys.getenv('OSF_PAT')), 'character')
+#   expect_is(login(), 'character')
+# })
 
 # test_that('welcome works', {
 #   expect_null(welcome())
