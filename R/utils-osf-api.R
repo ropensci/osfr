16154044a5486bd56b78f166daefdd05c57f1f8a
Aaron Wolen <aaron@wolen.com>
2018-10-22 13:17:47 -0400

    Add crul-based API client and helper functions

--- R/utils-osf-api.R
+++ R/utils-osf-api.R
@@ -20,3 +20,66 @@ construct_link <- function(request) {
 
   return(paste0(base, request))
 }
+
+
+#' Retrieve OSF personal access token.
+#'
+#' A OSF personal access token
+#' Looks in env var \code{OSF_PAT}
+#'
+#' @keywords internal
+#' @noRd
+osf_pat <- function(quiet = TRUE) {
+  pat <- Sys.getenv('OSF_PAT')
+  if (!nzchar(pat)) return(NULL)
+
+  if (!quiet) {
+    message("Using OSF PAT from envvar OSF_PAT")
+  }
+  pat
+}
+
+# Return a version specific user agent
+user_agent <- function(agent = "osfr") {
+  version <- system.file("DESCRIPTION", package = "osfr", mustWork = FALSE)
+  if (file.exists(version)) {
+    version <- base::read.dcf(version, "Version")
+    sprintf("%s v%s", agent, version)
+  } else {
+    agent
+  }
+}
+
+
+# Appends API version to a specificed path
+osf_path <- function(path) {
+  sprintf("v%s/%s", floor(.api_version), path)
+}
+
+# Construct the OSF API Client
+osf_cli <- function(pat = osf_pat()) {
+  server <- Sys.getenv('OSF_USE_SERVER')
+  url <- if (nzchar(server)) {
+    sprintf("https://api.%s.osf.io", server)
+  } else {
+    "https://api.osf.io"
+  }
+
+  headers <- list(
+    `User-Agent` = user_agent(),
+    `Accept-Header` = sprintf("application/vnd.api+json;version=%s", .api_version)
+  )
+
+  if (!is.null(pat)) {
+    headers$Authorization = sprintf('Bearer %s', pat)
+  }
+
+  crul::HttpClient$new(
+    url = url,
+    opts = list(
+      timeout = 5,
+      encode = "json"
+    ),
+    headers = headers
+  )
+}
