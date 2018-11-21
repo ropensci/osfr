b0638566c08c1e6a2862627d083a6f9390781e2e
Aaron Wolen <aaron@wolen.com>
2018-11-21 14:13:52 -0500

    Export osf_node_ls

--- R/node-list.R
+++ R/node-list.R
@@ -1,12 +1,15 @@
 # List all node children
 # @param id, a node ID, user ID, osf_tbl_node or osf_tbl_user
 # @returns osf_tbl_node
+#' @export
 osf_node_ls <- function(id, n_max = 10) {
   UseMethod("osf_node_ls")
 }
 
+#' @export
 osf_node_ls.character <- function(id, n_max = 10) osf_node_ls(as_id(id))
 
+#' @export
 osf_node_ls.osf_id <- function(id, n_max = 10) {
 
   # determine if provided ID maps to a user or node
@@ -22,11 +25,13 @@ osf_node_ls.osf_id <- function(id, n_max = 10) {
   osf_node_ls(out)
 }
 
+#' @export
 osf_node_ls.osf_tbl_node <- function(id, n_max = 10) {
   out <- .osf_node_children(as_id(id), n_max)
   as_osf_tbl_node(out)
 }
 
+#' @export
 osf_node_ls.osf_tbl_user <- function(id, n_max = 10) {
   out <- .osf_user_nodes(as_id(id), n_max)
   as_osf_tbl_node(out)
