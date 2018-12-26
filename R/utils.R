54c70c1917350fdd6549439e23a57f0a1fc979ab
Aaron Wolen <aaron@wolen.com>
2018-12-26 14:22:19 -0500

    Add make_single() (#5)

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,22 @@ extract_osf_id <- function(url) {
   path <- crul::url_parse(url)$path
   purrr::map_chr(fs::path_split(path), utils::tail, 1)
 }
+
+
+# Transparently filter user input to single object/row
+make_single <- function(x) {
+  if (inherits(x, "data.frame")) {
+    n <- nrow(x)
+    entity <- "row"
+  } else {
+    n <- length(x)
+    entity <- "element"
+  }
+
+  if (n > 1) {
+    msg <- "This is not a vectorized function. Only the first %s of %i will be used."
+    warn(sprintf(msg, entity, n))
+    x <- head(x, 1)
+  }
+  x
+}
