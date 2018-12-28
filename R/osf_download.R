66e6cf9f00eabde6354423c92f15370d7d42e8a3
Aaron Wolen <aaron@wolen.com>
2018-12-28 14:21:49 -0500

    Fix and export osf_download() (closes #4)

--- R/osf_download.R
+++ R/osf_download.R
@@ -1,21 +1,29 @@
-#' Download an OSF files or folder
+#' Download an OSF file or folder
 #'
 #' Files stored in OSF projects, components, or folders can be downloaded
 #' locally by providing the appropriate [`osf_tbl_file`] to `osf_download()`.
-#' Directories are downloaded as zip files.
+#' Note that directories are downloaded as zip files.
 #'
-#' @param an [`osf_tbl_file`] containing a file or directory
+#' @param x an [`osf_tbl_file`] containing a file or directory
 #' @param path Local path where the downloaded file will be saved. The default
 #'   is to use the remote file's name.
 #' @param overwrite Logical, if the local path already exists should it be
 #'   replaced with the downloaded file?
 #'
+#' @return `TRUE`, invisibly if download succeeded
+#' @examples
+#' \dontrun{
+#' analysis_plan <- osf_retrieve_file("79b2n")
+#' osf_download(analysis_plan)
+#' }
+#' @export
 #' @importFrom fs path_ext_set
 
 osf_download <- function(x, path = NULL, overwrite = FALSE) {
-  NextMethod("osf_download")
+  UseMethod("osf_download")
 }
 
+#' @export
 osf_download.osf_tbl_file <- function(x, path = NULL, overwrite = FALSE) {
   x <- make_single(x)
   if (is.null(path)) path <- x$name
