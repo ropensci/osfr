31e37b569f502c2ba5aa4850872e0bb409ae9f55
Aaron Wolen <aaron@wolen.com>
2018-12-29 10:55:54 -0500

    Add pattern filtering support to osf_ls_nodes()
    Closes (#17)

---
+++ man-roxygen/filter-pattern.R
@@ -0,0 +1,3 @@
+#' @param pattern character string used to filter for results that contain the
+#'   substring `"pattern"` in their name. Note this is a fixed, case-insensitive
+#'   search.
---
+++ man-roxygen/filter-type.R
@@ -0,0 +1,1 @@
+#' @param type filter query to include only `"file"` or `"folder"`
