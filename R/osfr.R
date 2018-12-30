785b02baaf4dc1be7fcac927df960f7feff86bd9
Aaron Wolen <aaron@wolen.com>
2018-12-30 14:21:05 -0500

    Add dplyr compatibility (closes #7)

--- R/osfr.R
+++ R/osfr.R
@@ -13,6 +13,6 @@
 #' @importFrom stringi stri_split_fixed
 #' @importFrom purrr map imap_chr pluck set_names transpose %||%
 #' @importFrom tibble tibble
-#' @importFrom rlang abort warn is_empty
+#' @importFrom rlang abort warn is_empty is_scalar_character
 #' @importFrom utils head tail modifyList browseURL
 NULL
--- tests/testthat/test-osf_tbl.R
+++ tests/testthat/test-osf_tbl.R
@@ -1,4 +1,4 @@
-context("osf_tbl class")
+context("osf_tbl construction")
 
 test_that("NULL input returns empty osf_tbl", {
   out <- osf_tbl()
@@ -11,3 +11,26 @@ test_that("empty list returns empty osf_tbl", {
   expect_s3_class(out, "osf_tbl")
   expect_equal(nrow(out), 0)
 })
+
+
+context("osf_tbl validation")
+
+user_tbl <- osf_retrieve_user("me")
+user_tbl$foo <- "bar"
+
+test_that("valid osf_tbls are passed through validation", {
+  expect_s3_class(rebuild_osf_tbl(user_tbl), "osf_tbl_user")
+})
+
+test_that("osf_tbls missing required columns are detected", {
+  expect_true(has_osf_tbl_colnames(user_tbl))
+  expect_false(has_osf_tbl_colnames(user_tbl[-1]))
+  expect_false(has_osf_tbl_colnames(user_tbl[-2]))
+  expect_false(has_osf_tbl_colnames(user_tbl[-3]))
+})
+
+test_that("osf_tbls with incorrect column types are detected", {
+  user_tbl$id <- as.factor(user_tbl$id)
+  expect_false(has_osf_tbl_coltypes(user_tbl))
+})
+
