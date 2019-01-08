4432d71f3e83f095647a8a7a16eb6925a050ae8b
Aaron Wolen <aaron@wolen.com>
2019-01-08 09:14:49 -0500

    Combine project/component tests

--- tests/testthat/test-projects.R
+++ tests/testthat/test-projects.R
@@ -1,7 +1,11 @@
 context("Project operations")
 
+
+# setup -------------------------------------------------------------------
 p1 <- osf_create_project(title = "osfr-project-tests")
 
+
+# tests -------------------------------------------------------------------
 test_that("create project", {
   expect_error(osf_create_project(), "Must define a title")
   expect_s3_class(p1, "osf_tbl_node")
@@ -11,4 +15,33 @@ test_that("project deletion", {
   expect_true(osf_rm(p1))
   out <- .osf_node_retrieve(p1$id)
   expect_equal(out$status_code, 410)
+})
+
+context("Component operations")
+
+c1 <- osf_create_component(p1, title = "component-1")
+
+
+# tests -------------------------------------------------------------------
+test_that("create component", {
+  expect_error(osf_create_component(), "`x` must be an `osf_tbl_node`")
+  expect_s3_class(c1, "osf_tbl_node")
+  expect_match(c1$id, osf_ls_nodes(p1)$id[1])
+})
+
+test_that("add nested components", {
+  c11 <- osf_create_component(c1, title = "component-1-1")
+  expect_s3_class(c11, "osf_tbl_node")
+  c12 <- osf_create_component(c1, title = "component-1-2")
+  expect_s3_class(c12, "osf_tbl_node")
+})
+
+test_that("deleting non-empty project/component fails", {
+  expect_error(osf_rm(p1), "Any child components must be deleted")
+  expect_error(osf_rm(c1), "Any child components must be deleted")
+})
+
+test_that("non-empty project can be recursively deleted", {
+  out <- osf_rm(p1, recursive = TRUE)
+  expect_true(out)
 })
