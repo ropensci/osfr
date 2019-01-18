9d413e48b8dd5aa9ff9a97d9d8b27b3acc86a797
Aaron Wolen <aaron@wolen.com>
2019-01-18 10:07:23 -0600

    Finish implementing osf_mv()

--- R/osf_mv.R
+++ R/osf_mv.R
@@ -4,7 +4,6 @@
 #' determines where the copy will be made.
 #'
 #' @param x An [`osf_tbl_file`] containing a single file or directory.
-#' @param path
 #' @param to The destination where the file or directory will be copied to. This
 #'   can be one of the following:
 #'   * An [`osf_tbl_node`] with a single project or component.
@@ -23,73 +22,55 @@ osf_mv <- function(x, to, overwrite = FALSE, verbose = FALSE) {
 
 #' @export
 osf_mv.osf_tbl_file <- function(x, to, overwrite = FALSE, verbose = FALSE) {
-
   x <- make_single(x)
-  id <- as_id(x)
+  out <- .wb_file_move(x, to, action = "move", overwrite = overwrite)
+  as_osf_tbl(out["data"], subclass = "osf_tbl_file")
+}
 
-  if (!inherits(to, c("osf_tbl_node", "osf_tbl_file"))) {
-    abort("Must pass an `osf_tbl_file` or `osf_tbl_node` to the `to` argument.")
-  }
 
-  if (inherits(to, "osf_tbl_file")) {
-    if (is_osf_file(to)) {
-      abort("If `to` is an `osf_tbl_file` it  must contain a directory, not a file.")
-    }
-    path <- get_meta(to, "attributes", "path")
-  } else {
-    # project/component path
-    path <- "/"
-  }
+#' Internal method for moving/copying files
+#' @noRd
+#' @references
+#' https://waterbutler.readthedocs.io/en/latest/api.html#actions
+.wb_file_move <- function(x, to, action, overwrite) {
+  action <- match.arg(action, c("move", "copy"))
+  conflict <- ifelse(overwrite, "replace", "warn")
 
   api_url <- get_meta(x, "links", "move")
   api_path <- crul::url_parse(api_url)$path
 
+  req <- modifyList(
+    build_move_request(to),
+    list(action = action, conflict = conflict)
+  )
+
+  res <- .wb_request("post", api_path, body = req, encode = "json")
+  out <- process_response(res)
+  raise_error(out)
+
+  # retrieve osf representation of file
   file_id <- strsplit(out$data$id, split = "/", fixed = TRUE)[[1]][2]
   .osf_file_retrieve(file_id)
 }
 
 
-.wb_file_move <- function(api_path, action, conflict) {
-  action <- match.arg(action, c("move", "copy"))
-  conflict <- match.arg(conflict, c("replace", "keep", "warn"))
-
-  if (to )
-
-
-  body <- list(
-    action = "move",
-    path = "5c410190154ce50017dc4da0/",
-    conflict = ifelse(overwrite, "replace", "warn")
-  )
-
-  res <- .wb_request("post", api_path, body = body, encode = "json")
-  out <- process_response(res)
-  raise_error(out)
-}
-
-
+# Construct the move/copy request's body
 build_move_request <- function(x) UseMethod("build_move_request")
 
 build_move_request.osf_tbl_file <- function(x) {
-  if (is_osf_file(to)) {
+  if (is_osf_file(x)) {
     abort("If `to` is an `osf_tbl_file` it  must contain a directory, not a file.")
   }
-
   list(
-    path = get_meta(to, "attributes", "path"),
-    body = list(
-
-    )
+    path = get_meta(x, "attributes", "path")
   )
-
 }
 
 build_move_request.osf_tbl_node <- function(x) {
   list(
     path = "/",
-    body = list(
-      resource = as_id(x)
-    )
+    resource = unclass(as_id(x)),
+    provider = "osfstorage"
   )
 }
 
