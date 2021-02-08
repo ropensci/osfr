3ebee26067e9d490cfd4dd24f7f727914b04bf6e
Aaron Wolen <aaron@wolen.com>
2021-02-08 10:14:12 -0600

    Use vcr for node tests

--- tests/testthat/test-nodes.R
+++ tests/testthat/test-nodes.R
@@ -2,9 +2,16 @@ context("Node creation")
 
 
 # setup -------------------------------------------------------------------
+
+vcr::vcr_configure(
+  dir = cassette_dir("nodes")
+)
+
 setup({
   if (has_pat()) {
-    p1 <<- osf_create_project(title = "osfr-test-nodes")
+    vcr::use_cassette("create-p1", {
+      p1 <<- osf_create_project(title = "osfr-test-nodes")
+    })
   }
 })
 
@@ -24,7 +31,10 @@ test_that("minimal project with default settings was created", {
 test_that("minimal component with default settings was created", {
   skip_if_no_pat()
 
-  c1 <<- osf_create_component(p1, title = "component-1")
+  vcr::use_cassette("create-c1", {
+    c1 <<- osf_create_component(p1, title = "component-1")
+  })
+
   expect_s3_class(c1, "osf_tbl_node")
   expect_false(get_meta(c1, "attributes", "public"))
   expect_match(c1$name, "component-1")
@@ -46,11 +56,15 @@ test_that("component creation errors with providing a parent node", {
 test_that("nested nodes can be created", {
   skip_if_no_pat()
 
-  c11 <- osf_create_component(c1, title = "component-1-1")
+  vcr::use_cassette("create-c11", {
+    c11 <- osf_create_component(c1, title = "component-1-1")
+  })
   expect_s3_class(c11, "osf_tbl_node")
   expect_match(get_parent_id(c11), c1$id)
 
-  c12 <- osf_create_component(c1, title = "component-1-2")
+  vcr::use_cassette("create-c12", {
+    c12 <- osf_create_component(c1, title = "component-1-2")
+  })
   expect_s3_class(c12, "osf_tbl_node")
   expect_match(get_parent_id(c12), c1$id)
 })
@@ -78,8 +92,11 @@ test_that("an invalid or ambiguous category errors", {
 test_that("a valid category can be specified", {
   skip_if_no_pat()
 
-  p2 <<- osf_create_project("osfr-project-category-test", category = "Analysis")
-  c2 <- osf_create_component(p2, "osfr-component-category-test", category = "Data")
+  vcr::use_cassette("create-p2-and-c2", {
+    p2 <<- osf_create_project("osfr-project-category-test", category = "Analysis")
+    c2 <- osf_create_component(p2, "osfr-component-category-test", category = "Data")
+  })
+
   expect_match(get_meta(p2, "attributes", "category"), "analysis")
   expect_match(get_meta(c2, "attributes", "category"), "data")
 })
@@ -90,15 +107,28 @@ context("Node deletion")
 test_that("deleting non-empty project/component fails", {
   skip_if_no_pat()
 
-  expect_error(osf_rm(p1, check = FALSE), "Any child components must be deleted")
-  expect_error(osf_rm(c1, check = FALSE), "Any child components must be deleted")
+  expect_error(
+    vcr::use_cassette("delete-p1-error", {
+      osf_rm(p1, check = FALSE)
+    }),
+    "Any child components must be deleted"
+  )
+  expect_error(
+    vcr::use_cassette("delete-c1-error", {
+      osf_rm(c1, check = FALSE)
+    }),
+    "Any child components must be deleted"
+  )
 })
 
 test_that("non-empty project can be recursively deleted", {
   skip_if_no_pat()
 
-  out <- osf_rm(p1, recurse = TRUE, check = FALSE)
-  expect_true(out)
-  out <- osf_rm(p2, recurse = TRUE, check = FALSE)
-  expect_true(out)
+  vcr::use_cassette("delete-p1-success", {
+    expect_true(osf_rm(p1, recurse = TRUE, check = FALSE))
+  })
+
+  vcr::use_cassette("delete-p2-success", {
+    expect_true(osf_rm(p2, recurse = TRUE, check = FALSE))
+  })
 })
