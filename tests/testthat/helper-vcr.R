3d94e2662a3929c6a7f108e1b1ef0f2803b706e5
Aaron Wolen <aaron@wolen.com>
2026-02-22 08:48:10 -0600

    Ensure OSF_SERVER is always defined for tests

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,5 +1,10 @@
 library(vcr)
 
+# Cassettes are recorded against the test server so ensure OSF_SERVER is set
+if (!nzchar(Sys.getenv("OSF_SERVER"))) {
+  withr::local_envvar(OSF_SERVER = "test", .local_envir = teardown_env())
+}
+
 cassette_dir <- function(x) {
   vcr::vcr_test_path("cassettes", x)
 }
