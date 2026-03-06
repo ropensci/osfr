28d0e7fa2eec94df9765f97f731d127c1551ac18
Aaron Wolen <aaron@wolen.com>
2026-03-05 20:19:32 -0600

    Move test env var setup from helper to setup.R to fix load_all()

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,10 +1,5 @@
 library(vcr)
 
-# Cassettes are recorded against the test server so ensure OSF_SERVER is set
-if (!nzchar(Sys.getenv("OSF_SERVER"))) {
-  withr::local_envvar(OSF_SERVER = "test", .local_envir = teardown_env())
-}
-
 cassette_dir <- function(x) {
   vcr::vcr_test_path("cassettes", x)
 }
